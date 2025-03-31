6a686eca0f1302d8b1d3282a5fb9e65d4dd08176
Kenneth Lowe <kennethlowe45@yahoo.COM>
2025-03-30 21:17:43 -0500

    first Main commit

--- news-letter/src/components/Main/Main.jsx
+++ news-letter/src/components/Main/Main.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import authorImg from "../../assets/author.jpeg";
+import "./Main.css";
 
 function Main() {
   return (
     <main className="main">
-      <h2>About the author</h2>
-      <p>
-        This block shows some random infor about the author you had picked for
-        your news litter
-      </p>
-      <p>Just another p tag for the formatting of this site.</p>
+      <img className="main__author-img" src={authorImg} alt="Author" />
+      <div className="main__content">
+        <h2 className="main__title">About the author</h2>
+        <p className="main__description">
+          This block shows some random infor about the author you had picked for
+          your news litter
+        </p>
+        <p>Just another p tag for the formatting of this site.</p>
+      </div>
     </main>
   );
 }
