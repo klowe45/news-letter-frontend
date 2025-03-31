f52e936e045d54a9cfd9f1fb544c3f18517a3e65
Kenneth Lowe <kennethlowe45@yahoo.COM>
2025-03-30 22:02:42 -0500

    footer formated

--- news-letter/src/components/Footer/Footer.jsx
+++ news-letter/src/components/Footer/Footer.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
-      <p>2025 News Letter Site</p>
-      <p>Home</p>
-      <p>TripleTen</p>
+      <div className="footer__content">
+        <p className="footer__info">&copy; 2025 News Letter Site</p>
+        <p className="footer__home-btn">Home</p>
+        <p className="footer__school">TripleTen</p>
+        <button className="footer__github-btn"></button>
+        <button className="footer__linkedin-btn"></button>
+      </div>
     </footer>
   );
 }
