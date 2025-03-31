4a204639da0f6beb0175198f86a5f8ecfe867590
Kenneth Lowe <kennethlowe45@yahoo.COM>
2025-03-30 22:22:56 -0500

    footer links added

--- news-letter/src/App.jsx
+++ news-letter/src/App.jsx
@@ -4,11 +4,26 @@ import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
 function App() {
+  /***************************************************************************
+   *                                  Footer                                 *
+   **************************************************************************/
+
+  const handleGithubClick = () => {
+    window.open("https://github.com/klowe45");
+  };
+
+  const handleLinkedinClick = () => {
+    window.open("https://www.linkedin.com/in/kenneth-lowe45");
+  };
+
   return (
     <div className="page__content">
       <Header />
       <Main />
-      <Footer />
+      <Footer
+        handleGithubClick={handleGithubClick}
+        handleLinkedinClick={handleLinkedinClick}
+      />
     </div>
   );
 }
--- news-letter/src/components/Footer/Footer.jsx
+++ news-letter/src/components/Footer/Footer.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Footer.css";
 
-function Footer() {
+function Footer({ handleGithubClick, handleLinkedinClick }) {
   return (
     <footer className="footer">
       <div className="footer__content">
         <p className="footer__info">&copy; 2025 News Letter Site</p>
         <p className="footer__home-btn">Home</p>
         <p className="footer__school">TripleTen</p>
-        <button className="footer__github-btn"></button>
-        <button className="footer__linkedin-btn"></button>
+        <button
+          className="footer__github-btn"
+          onClick={handleGithubClick}
+        ></button>
+        <button
+          className="footer__linkedin-btn"
+          onClick={handleLinkedinClick}
+        ></button>
       </div>
     </footer>
   );
