74da72eb1e3fa98e1594d44ba08af01d732844eb
Kenneth Lowe <kennethlowe45@yahoo.COM>
2025-05-19 18:21:16 -0500

    more views added

--- news-letter/src/components/About/About.jsx
+++ news-letter/src/components/About/About.jsx
@@ -9,12 +9,14 @@ function About() {
       <div className="about__content">
         <h2 className="about__title">About the author</h2>
         <p className="about__description">
-          This block shows some random infor about the author you had picked for
-          your news litter
-        </p>
-        <p className="about__description">
-          Just another p tag for the formatting of this site. And other really
-          cool stuff.
+          My name is Kenneth Lowe, and I’m the creator of this website, which I
+          built using the skills and knowledge I gained through my time with
+          TripleTen. One of the key practices in this project was the use of
+          components and the effective passing of data between them. Managing
+          the state of various elements played a crucial role in building a
+          functional and responsive site. The technical abilities I’ve
+          developed—along with my problem-solving skills and
+          resourcefulness—will be valuable assets to any future employer.
         </p>
       </div>
     </main>
